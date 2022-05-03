6ebdfdf8cf4157c1fa262404c76f50f352d9642c
penguliar <103967583+penguliar@users.noreply.github.com>
2022-05-03 14:42:40 -0700

    5/3/2022 2:42 PM

--- Assets/Scripts/House/InteractScript.cs
+++ Assets/Scripts/House/InteractScript.cs
@@ -7,6 +7,8 @@ public class InteractScript : MonoBehaviour
     public GameObject[] interactables;
     public int currentInteractable = 0;
 
+    public GameObject watchNotifications;
+
     // Update is called once per frame
     void Update()
     {
@@ -14,6 +16,8 @@ public class InteractScript : MonoBehaviour
         {
             if (interactables[currentInteractable].GetComponent<Trigger>().entered == true && Input.GetButtonDown("Interact"))
             {
+                watchNotifications.GetComponent<Notifications>().notiNum += 1;
+
                 currentInteractable += 1;
             }
         }
