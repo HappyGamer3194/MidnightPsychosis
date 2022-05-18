71228a7c9f2c5daf07acaa285e8ed3185b56c304
penguliar <103967583+penguliar@users.noreply.github.com>
2022-05-18 15:57:52 -0700

    3:54 PM 5/18/2022

--- Assets/Scripts/House/InteractScript.cs
+++ Assets/Scripts/House/InteractScript.cs
@@ -6,8 +6,9 @@ using UnityEngine.Animations;
 public class InteractScript : MonoBehaviour
 {
     public Interactables[] interactables;
-    public int currentInteractable = 0;
+    int currentInteractable = 0;
     bool interacted;
+    public float pickupTime;
 
     public GameObject watchNotifications;
 
@@ -17,30 +18,71 @@ public class InteractScript : MonoBehaviour
         if (interactables[currentInteractable].interactable.GetComponent<Trigger>().entered == true && Input.GetButtonDown("Interact"))
         {
             interacted = true;
-
-            if (interactables[currentInteractable].animator != null)
-            {
-                interactables[currentInteractable].animator.SetBool("Activate", true);
-            }
-
             if (interactables[currentInteractable].portable == false)
             {
-                if (currentInteractable + 1 < interactables.Length && interactables[currentInteractable].portable == false)
+                if (currentInteractable + 1 < interactables.Length)
                 {
-                    watchNotifications.GetComponent<Notifications>().notiNum += 1;
-                    currentInteractable += 1;
-                    Debug.Log("Task Completed");
+                    TaskCompleted();
                 }
-                else if (interactables[currentInteractable].portable == false)
+                else
                 {
                     watchNotifications.GetComponent<Notifications>().notiNum += 1;
                     Debug.Log("All tasks completed");
                 }
             }
         }
-        if (interactables[currentInteractable].portable == true && interacted == true)
+        if (interacted == true)
         {
-            interactables[currentInteractable].interactable.transform.position = GameObject.FindGameObjectWithTag("MainCamera").transform.position + interactables[currentInteractable].offset;
+            if (interactables[currentInteractable].changeMaterial == true)
+            {
+                interactables[currentInteractable].currentChangeTime += Time.fixedDeltaTime;
+            }
+
+            if (interactables[currentInteractable].animator != null)
+            {
+                interactables[currentInteractable].animator.SetBool("Activate", true);
+            }
+            if (interactables[currentInteractable].portable == true)
+            {
+                interactables[currentInteractable].interactable.transform.localPosition = Vector3.Slerp(interactables[currentInteractable].interactable.transform.localPosition, interactables[currentInteractable].offset, pickupTime);
+                interactables[currentInteractable].interactable.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+            }
         }
+
+        //If the currentChangeTime is more that the changeTime
+
+        if (interactables[currentInteractable].currentChangeTime > interactables[currentInteractable].changeTime)
+        {
+            //Change the material to the interacted material
+            interactables[currentInteractable].interactable.GetComponent<MeshRenderer>().material = interactables[currentInteractable].material;
+        }
+        if (interactables[currentInteractable].animator != null)
+        {
+            if (interactables[currentInteractable].animator.GetCurrentAnimatorStateInfo(0).IsName("Finished"))
+            {
+                TaskCompleted();
+            }
+        }
+    }
+
+    void TaskCompleted()
+    {
+        if (interactables[currentInteractable].portable)
+        {
+            interactables[currentInteractable].interactable.transform.localPosition = Vector3.Slerp(interactables[currentInteractable].interactable.transform.localPosition, interactables[currentInteractable].offset, pickupTime * Time.fixedDeltaTime);
+            if (interactables[currentInteractable].endingLocation != null)
+            {
+                interactables[currentInteractable].interactable.transform.position = interactables[currentInteractable].endingLocation;
+            } else
+            {
+                Debug.Log("endingPosition for " + interactables[currentInteractable].interactable.name + " is not set");
+            }
+            interactables[currentInteractable].interactable.transform.parent = null;
+        }
+        watchNotifications.GetComponent<Notifications>().notiNum += 1;
+        currentInteractable += 1;
+        Debug.Log("Task Completed");
+
+        interacted = false;
     }
 }
--- Assets/Scripts/House/InteractablesClass.cs
+++ Assets/Scripts/House/InteractablesClass.cs
@@ -8,11 +8,35 @@ public class Interactables
     public GameObject interactable;
     public Animator animator;
 
+    //Freezes player on object interaction
+    [Header("Freeze State")]
+    public bool freezeState;
+    [ConditionalHide("freezeState", true)]
+    [Tooltip("Position the player will freeze in")]
+    public Vector3 freezePosition;
+    [ConditionalHide("freezeState", true)]
+    [Tooltip("How long the transition should be, 1 < time; 0.1 > time")]
+    public float freezeTransition;
+    [ConditionalHide("freezeState", true)]
+    public float freezeDuration;
+
+    //Changes objects material
+    [Header("Material")]
+    public bool changeMaterial;
+    [ConditionalHide("changeMaterial", true)]
+    public Material material;
+    [ConditionalHide("changeMaterial", true)]
+    [Tooltip("Time until material is changed")]
+    public float changeTime;
+    [ConditionalHide("changeMaterial", true)]
+    [Tooltip("Current time since object has been interacted with")]
+    public float currentChangeTime;
+
+    //Checks if the object is able to be picked up
     [Header("Portability")]
     public bool portable;
-
     [ConditionalHide("portable", true)]
-    public Transform startingLocation;
+    public Vector3 endingLocation;
     [ConditionalHide("portable", true)]
     public Vector3 offset;
 }
