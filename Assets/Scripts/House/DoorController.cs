fcdacdcc46badbb87cac9cb35d643708bac036d1
HappyGamer3194 <happygamer3194@gmail.com>
2022-05-21 15:53:04 -0700

    finished more room, fixed parents note, fixed some layout and scaling issues

--- Assets/Scripts/House/DoorController.cs
+++ Assets/Scripts/House/DoorController.cs
@@ -9,18 +9,38 @@ public class DoorController : MonoBehaviour
     public Animator doorAnimation;
     public bool isDoorEnabled = true;
 
+    private Collider doorCollider;
+
+    private void Start()
+    {
+        doorCollider = gameObject.GetComponent<Collider>();
+    }
+
     private void Update()
     {
         if (trigger.GetComponent<Trigger>().entered == true && Input.GetButtonDown("Interact") && isDoorEnabled)
         {
-            if(doorAnimation.GetBool("openClose") == false)
-            {
-                doorAnimation.SetBool("openClose", true);
-            }
-            else if(doorAnimation.GetBool("openClose") == true)
-            {
-                doorAnimation.SetBool("openClose", false);
-            }
+            StartCoroutine(DoorAnimation());
         }
+    }
+
+    private IEnumerator DoorAnimation()
+    {
+        doorCollider.enabled = false;
+
+        yield return new WaitForSeconds(0.1f);
+
+        if (doorAnimation.GetBool("openClose") == false)
+        {
+            doorAnimation.SetBool("openClose", true);
+        }
+        else if (doorAnimation.GetBool("openClose") == true)
+        {
+            doorAnimation.SetBool("openClose", false);
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        doorCollider.enabled = true;
     }
 }
