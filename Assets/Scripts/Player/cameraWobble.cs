8583083de7888e77d0a109a80507083f6de99c7c
penguliar <103967583+penguliar@users.noreply.github.com>
2022-05-25 17:42:27 -0700

    5:42

--- Assets/Scripts/Player/cameraWobble.cs
+++ Assets/Scripts/Player/cameraWobble.cs
@@ -5,14 +5,19 @@ using UnityEngine.Audio;
 
 public class cameraWobble : MonoBehaviour
 {
+    public AudioSource sound;
+
     public float amount = 10.0f;
     public float speed = 1.0f;
     private Vector3 lastPos;
     private float dist = 0.0f;
     private Vector3 rotation = Vector3.zero;
 
+    GameObject player;
+
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         lastPos = transform.position;
     }
 
@@ -22,5 +27,17 @@ public class cameraWobble : MonoBehaviour
         lastPos = transform.position;
         rotation.z = Mathf.Sin(dist * speed) * amount;
         transform.localEulerAngles = rotation;
+
+        if (player.GetComponent<CharacterController>().velocity.magnitude > 0f)
+        {
+            Debug.Log(player.GetComponent<CharacterController>().velocity.magnitude);
+        }
+
+        if (player.GetComponent<CharacterController>().velocity.magnitude > 0.3f && sound.isPlaying == false)
+        {
+            sound.volume = Random.Range(0.8f, 1);
+            sound.pitch = Random.Range(0.8f, 1.1f);
+            sound.Play();
+        }
     }
 }
